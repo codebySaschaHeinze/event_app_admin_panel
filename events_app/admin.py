5749bc17d35d7f5d17731be740002b28826d6051
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-16 13:55:56 +0100

    feat(event_app): add and set up list/search/field sets (Event).

--- events_app/admin.py
+++ events_app/admin.py
@@ -3,8 +3,24 @@ from .models import EventCategory, Location, Event
 
 # Register your models here.
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'location', 'date')
+    search_fields = ('title', 'date')
+    list_filter = ('category')
+
+    fieldsets = (
+        ('Allgemein',{
+            'fields': ('title', 'category', 'date'),
+        }),
+
+        ('Organisation', {
+            'fields': ('location', 'capacity'),
+        })
+    )
+
 admin.site.register(EventCategory)
 
 admin.site.register(Location)
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
