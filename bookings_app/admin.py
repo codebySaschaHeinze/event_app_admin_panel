10689718c321201deb3c2992c2404326b1998c3e
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-16 14:46:07 +0100

    feat(bookings_app): set list attributes (admin.py).

--- bookings_app/admin.py
+++ bookings_app/admin.py
@@ -3,6 +3,10 @@ from .models import Participant, Booking
 
 # Register your models here.
 
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("participant", "event", "confirmed", "booking_date")
+    list_filter = ['confirmed',]
+
 admin.site.register(Participant)
 
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
