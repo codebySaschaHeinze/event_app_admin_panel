845d477847e5d93a75a26e6dd0388a832f5a8fad
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-16 13:14:19 +0100

    feat(events_app): register models in admin panel.

--- events_app/admin.py
+++ events_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import EventCategory, Location, Event
 
 # Register your models here.
+
+admin.site.register(EventCategory)
+
+admin.site.register(Location)
+
+admin.site.register(Event)
